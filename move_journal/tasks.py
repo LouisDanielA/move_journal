6bf0ed9571df28f7f8ff3afc640f07fab18f186d
LouisDanielA <82762800+LouisDanielA@users.noreply.github.com>
2022-10-20 20:58:26 +0600

    Update tasks.py

--- move_journal/tasks.py
+++ move_journal/tasks.py
@@ -32,7 +32,7 @@ def check_data():
         subject = 'Добрый день, начальник!'
         message = 'Не заполнены, некоторые поля за вчерашний день!'
         email_from = settings.EMAIL_HOST_USER
-        recipient_list = ['MizanbaevAE@polymetal.kz', ]
+        recipient_list = ['', ]
         send_mail(subject, message, email_from, recipient_list)
     
 
